61d1a5ca5b54e46a1179f4b8be59a8f4072e8d74
kishan-dhankecha <dhankechakishan@gmail.com>
2021-12-30 21:21:09 +0530

    build upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -16,10 +16,10 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ac4cfb25093a875832926b108436b475",
-"/": "ac4cfb25093a875832926b108436b475",
+"index.html": "ff87c21659b95137b40ca9dc8a28455b",
+"/": "ff87c21659b95137b40ca9dc8a28455b",
 "main.dart.js": "74001dc4a9a9058b6e7b84a8fb17a1b2",
-"manifest.json": "a9733c53596bf0a0292009f1c78743f1",
+"manifest.json": "d0b95d5e89fcf7852049e647df6cc433",
 "splash/style.css": "288e11ff34f12039634a05e1ca1a97bd",
 "version.json": "9046f2378c956f2dd92873d506469675"
 };
